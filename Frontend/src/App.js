1238c6356c2449b2049333213878ca4a69e24c18
willianmendesf <robertinho1190@gmail.com>
2020-07-31 22:32:46 -0300

    GoStack FrontEnd Ended

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,26 +1,39 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from './services/api'
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [repositories])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', {
+      title: `Repository ${Date.now()}`,
+      techs: [ 'Angular1','VueJS1','Java1' ]
+    })
+
+    const repository = response.data
+    setRepositories([...repositories, repository])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`/repositories/${id}`)
   }
+
+  async function handleAddLikes (id) {
+    api.post(`/repositories/${id}/like`)
+  }
+  
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        { repositories.map(repository => <li key={repository.id}><p>Title: { repository.title } <span>Likes: { repository.likes } <button onClick={()=>handleAddLikes(repository.id)}>Curtir</button> </span> </p> <button onClick={()=>handleRemoveRepository(repository.id)}>Apagar</button></li>) }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
